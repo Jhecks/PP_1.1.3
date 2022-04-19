6523364c03be05241078144d0ee8d6be25fbc3de
Michael <semenov.yuri.michael@gmail.com>
2022-04-19 16:11:11 +0300

    Rework

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -10,41 +10,87 @@ import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
     private final String TABLE_NAME = "users_db.users";
-    private Statement statement;
 
     public UserDaoJDBCImpl() {}
 
     public void createUsersTable() {
-        String command = "CREATE TABLE IF NOT EXISTS " + TABLE_NAME + " ( " +
+        String command = String.format("CREATE TABLE IF NOT EXISTS %s (" +
                 "`id` BIGINT NOT NULL AUTO_INCREMENT, " +
                 "`name` VARCHAR(255) NOT NULL, " +
                 "`last_name` VARCHAR(255) NOT NULL, " +
                 "`age` TINYINT NOT NULL, " +
-                "PRIMARY KEY (`id`))";
-        execute(command);
+                "PRIMARY KEY (`id`))", TABLE_NAME);
+
+        try (Connection connection = new Util().getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(command)) {
+            preparedStatement.execute();
+
+        } catch (SQLException e) {
+            System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
+
     public void dropUsersTable() {
-        String command = "DROP TABLE IF EXISTS " + TABLE_NAME + ";";
-        execute(command);
+        String command = String.format("DROP TABLE IF EXISTS %s;", TABLE_NAME);
+
+        try (Connection connection = new Util().getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(command)) {
+            preparedStatement.execute();
+        } catch (SQLTimeoutException e) {
+            System.err.println("Timeout exception was caught");
+            System.err.println(Arrays.toString(e.getStackTrace()));
+        } catch (SQLException e) {
+            System.err.println(Arrays.toString(e.getStackTrace()));
+        } catch (Exception e) {
+            System.err.println("Unable to connect to database");
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        String command = String.format("INSERT INTO %s (name, last_name, age) VALUES (\"%s\", \"%s\", %d)",
-                TABLE_NAME, name, lastName, age);
-        execute(command);
+        String command = String.format("INSERT INTO %s (name, last_name, age) VALUES (?, ?, ?)", TABLE_NAME);
+
+        try (Connection connection = new Util().getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(command)) {
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, lastName);
+            preparedStatement.setInt(3, age);
+            preparedStatement.execute();
+        } catch (SQLTimeoutException e) {
+            System.err.println("Timeout exception was caught");
+            System.err.println(Arrays.toString(e.getStackTrace()));
+        } catch (SQLException e) {
+            System.err.println(Arrays.toString(e.getStackTrace()));
+        } catch (Exception e) {
+            System.err.println("Unable to connect to database");
+        }
     }
 
     public void removeUserById(long id) {
-        String command = String.format("DELETE FROM %s WHERE id=%d;", TABLE_NAME, id);
-        execute(command);
+        String command = String.format("DELETE FROM %s WHERE id=?;", TABLE_NAME);
+
+        try (Connection connection = new Util().getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(command)) {
+            preparedStatement.setLong(1, id);
+            preparedStatement.execute();
+        } catch (SQLTimeoutException e) {
+            System.err.println("Timeout exception was caught");
+            System.err.println(Arrays.toString(e.getStackTrace()));
+        } catch (SQLException e) {
+            System.err.println(Arrays.toString(e.getStackTrace()));
+        } catch (Exception e) {
+            System.err.println("Unable to connect to database");
+        }
     }
 
     public List<User> getAllUsers() {
+        String command = String.format("SELECT * FROM %s;", TABLE_NAME);
         List<User> result = new ArrayList<>();
-        try (Util util = new Util()) {
-            statement = util.getConnection();
-            String command = "SELECT * FROM " + TABLE_NAME + ";";
-            ResultSet resultSet = statement.executeQuery(command);
+
+        try (Connection connection = new Util().getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(command)) {
+            ResultSet resultSet = preparedStatement.executeQuery();
             while (resultSet.next()) {
                 User user = new User(resultSet.getString("name"),
                         resultSet.getString("last_name"), resultSet.getByte("age"));
@@ -63,14 +109,11 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void cleanUsersTable() {
-        String command = "TRUNCATE TABLE " + TABLE_NAME + ";";
-        execute(command);
-    }
+        String command = String.format("TRUNCATE TABLE %s;", TABLE_NAME);
 
-    private void execute(String command) {
-        try (Util util = new Util()) {
-            statement = util.getConnection();
-            statement.executeUpdate(command);
+        try (Connection connection = new Util().getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(command)) {
+            preparedStatement.execute();
         } catch (SQLTimeoutException e) {
             System.err.println("Timeout exception was caught");
             System.err.println(Arrays.toString(e.getStackTrace()));
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -4,26 +4,26 @@ import java.sql.*;
 
 public class Util implements AutoCloseable {
     private final Connection connection;
-    private final Statement statement;
 
     public Util() {
         try {
             // Connecting private data is in separate file Config, that is in gitIgnore (schema name is: users_db)
             connection = DriverManager.getConnection(
                     Config.DB_URL, Config.USER, Config.PASS);
-            statement = connection.createStatement();
         } catch (Exception e) {
             throw new DatabaseConnectionException("Database connection error");
         }
     }
 
-    public Statement getConnection() {
-        return statement;
+    public Connection getConnection() {
+        return connection;
+
     }
 
     @Override
     public void close() throws Exception {
-        connection.close();
+        if (connection != null)
+            connection.close();
     }
 
     public static class DatabaseConnectionException extends RuntimeException {
